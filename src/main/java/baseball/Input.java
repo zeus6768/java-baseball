e56ea2b52b6cc6711707a8a4bb9c94da0587c4b3
Zeus6768 <akaps6768@gmail.com>
2023-10-18 12:25:24 +0900

    feat(input): 숫자 입력 검증
    1. 세글자
    2. 숫자
    3. 0을 포함하지 않음
    4. 중복된 숫자 없음

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -8,5 +8,36 @@ public class Input {
 
     protected static void guess() {
         userGuess = Console.readLine();
+        validateUserGuess();
+    }
+
+    private static void validateUserGuess() {
+        if (userGuess.length() != 3) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (!isNumeric()) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (userGuess.contains("0")) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+        if (isDuplicate()) {
+            throw new IllegalArgumentException(InputErrorMessage.GUESS);
+        }
+    }
+
+    private static boolean isNumeric() {
+        for (char c : userGuess.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDuplicate() {
+        return userGuess.charAt(0) == userGuess.charAt(1)
+                || userGuess.charAt(1) == userGuess.charAt(2)
+                || userGuess.charAt(2) == userGuess.charAt(0);
     }
 }
--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,18 +1,17 @@
 package baseball;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest {
 
-    @DisplayName("인게임 입력: 정상 입력")
     @Test
-    void 정상_입력() {
+    void 게임_정상_입력() {
         String input = "123";
         setInput(input);
 
@@ -20,6 +19,37 @@ public class InputTest {
 
         assertThat(Input.userGuess).isEqualTo(input);
     }
+
+    @Test
+    void 게임_세자리_숫자가_아닌_입력시_예외() {
+        String input = "하나둘";    // "하나둘", "#$@", ""
+        setInput(input);
+
+        assertThatThrownBy(Input::guess)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputErrorMessage.GUESS);
+    }
+
+    @Test
+    void 게임_중복된_숫자_입력시_예외() {
+        String input = "111";
+        setInput(input);
+
+        assertThatThrownBy(Input::guess)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputErrorMessage.GUESS);
+    }
+
+    @Test
+    void 게임_입력에_0_포함시_예외() {
+        String input = "305";   // "012", "890"
+        setInput(input);
+
+        assertThatThrownBy(Input::guess)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputErrorMessage.GUESS);
+    }
+
 
     protected void setInput(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
